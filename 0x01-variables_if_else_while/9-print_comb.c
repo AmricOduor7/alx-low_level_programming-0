1afe4829bb32af8f852e0023da4d409bb3b2da4d
Your Name <you@example.com>
2023-02-18 01:18:06 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -function
+ *
+ * Description: prints all possible cobinations of single-digit numbers
+ *
+ * Return: returns (0) on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
